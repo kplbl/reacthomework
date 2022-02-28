9c79c0b4271ce77def98a4f56cad50dd7e65860f
kplbl <51096422+kplbl@users.noreply.github.com>
2022-02-28 18:57:44 +0100

    header mock

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <header>
+        <nav>
+          <div className="nav__logo">TMDB</div>
+          <div className="nav__link">Movies</div>
+          <div className="nav__link">TV Shows</div>
+          <div className="nav__link">People</div>
+          <div className="nav__link">More</div>
+        </nav>
+        <div className="header__extra">
+          <div>Login</div>
+          <div>Join TMDB</div>
+          <div>Search</div>
+        </div>
+      </header>
+      <footer></footer>
+    </div>
+  );
 }
 
 export default App;
