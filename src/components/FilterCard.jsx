c90faba68c4224e16cde82f366f9648b71ffe9b5
kplbl <51096422+kplbl@users.noreply.github.com>
2022-03-01 20:57:34 +0100

    basic layout

--- src/components/FilterCard.jsx
+++ src/components/FilterCard.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { ChevronRightIcon } from "@heroicons/react/outline";
 
 function FilterCard() {
-  return <div>FilterCard</div>;
+  return (
+    <div className="main__aside__card">
+      <div className="main__aside__card__box">
+        <div className="main__aside__card__box__text">Filters</div>
+
+        <ChevronRightIcon className="main__aside__card__box__icon" />
+      </div>
+
+      <div></div>
+    </div>
+  );
 }
 
 export default FilterCard;
