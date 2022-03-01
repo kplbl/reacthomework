f2062821450a5fe851f6412241ae7e3ddfe8f9a7
kplbl <51096422+kplbl@users.noreply.github.com>
2022-03-01 20:22:50 +0100

    english title

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import {
+  CircularProgressbarWithChildren,
+  buildStyles,
+} from "react-circular-progressbar";
 
 function MovieCard(props) {
-  const { original_title, release_date, vote_average, poster_path } =
-    props.movie;
+  const { title, release_date, vote_average, poster_path } = props.movie;
 
   return (
     <div className="moviecard">
@@ -10,7 +13,24 @@ function MovieCard(props) {
         <img src={`https://image.tmdb.org/t/p/original${poster_path}`} alt="" />
       </div>
       <div className="moviecard__content">
-        <h2>{original_title}</h2>
+        <div className="moviecard__content__circle">
+          <CircularProgressbarWithChildren
+            value={vote_average}
+            maxValue={10.0}
+            styles={buildStyles({
+              pathColor: "#21c774",
+
+              trailColor: "#112b24",
+              backgroundColor: "#000000",
+            })}
+          >
+            <div>
+              {`${Number(vote_average) * 10.0}`}
+              <span>%</span>
+            </div>
+          </CircularProgressbarWithChildren>
+        </div>
+        <h2>{title}</h2>
         <p>
           {new Date(release_date).toLocaleString("en-US", {
             month: "short",
@@ -18,9 +38,6 @@ function MovieCard(props) {
             year: "numeric",
           })}
         </p>
-        <div className="moviecard__percent">
-          <div className="circle"></div>
-        </div>
       </div>
     </div>
   );
