e324d5fac779ebfcc520bfea4e206d9d2fcc52ac
kplbl <51096422+kplbl@users.noreply.github.com>
2022-03-01 21:08:58 +0100

    filter card layout

--- src/components/FilterCard.jsx
+++ src/components/FilterCard.jsx
@@ -1,16 +1,48 @@
 import React from "react";
+import { useState } from "react";
 import { ChevronRightIcon } from "@heroicons/react/outline";
 
 function FilterCard() {
+  const [open, setOpen] = useState(false);
+
+  const toggleOpen = () => {
+    setOpen((state) => !state);
+  };
+
+  const genres = [
+    "action",
+    "adventure",
+    "animation",
+    "comedy",
+    "crime",
+    "documentary",
+    "drama",
+    "family",
+    "fantasy",
+    "history",
+    "horror",
+    "music",
+    "mystery",
+    "romance",
+    "science fiction",
+    "tv movie",
+    "thriller",
+    "war",
+    "western",
+  ];
+
   return (
     <div className="main__aside__card">
       <div className="main__aside__card__box">
         <div className="main__aside__card__box__text">Filters</div>
 
-        <ChevronRightIcon className="main__aside__card__box__icon" />
+        <ChevronRightIcon
+          onClick={toggleOpen}
+          className="main__aside__card__box__icon"
+        />
       </div>
 
-      <div></div>
+      <div className={`${!open && "hide"}`}>Test</div>
     </div>
   );
 }
